7cac70146b9d51364b6072d4548b6017a4492be1
fauzigalih <fauzigalihas@gmail.com>
2020-06-27 19:44:08 +0700

    Update model class

---
+++ Paket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dial_Up_Project
+{
+    class Paket
+    {
+        public int PaketID { get; set; }
+        public string PaketName { get; set; }
+        public string PaketDate { get; set; }
+
+        public int paketTelkomsayID = 1;
+        public int paketIndosayID = 2;
+    }
+}
